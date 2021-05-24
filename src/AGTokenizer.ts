eacfecaac263cdc5e49e9c66b83e3ca6afd7f293
Alex Kwak <take-me-home@kakao.com>
2021-05-24 14:57:26 +0900

    Initial commit

---
+++ src/AGTokenizer.ts
@@ -0,0 +1,40 @@
+import {InputStream} from 'antlr4'
+import {languages} from 'monaco-editor'
+import {CypherLexer} from "cypher-editor-support/src/_generated/CypherLexer";
+
+class CypherState implements languages.IState {
+    clone() {
+        return new CypherState()
+    }
+
+    equals() {
+        return true
+    }
+}
+
+export class CypherTokensProvider implements languages.TokensProvider {
+    getInitialState(): CypherState {
+        return new CypherState()
+    }
+
+    tokenize(line: string): languages.ILineTokens {
+        const lexer = new CypherLexer(new InputStream(line))
+
+        return {
+            endState: new CypherState(),
+            tokens: lexer
+                .getAllTokens()
+                .filter((token) => token !== null && token.type !== -1)
+                .map((token) => ({
+                    scopes:
+                        (
+                            CypherLexer.symbolicNames[token.type] ??
+                            CypherLexer.literalNames[token.type] ??
+                            ''
+                        ).toLowerCase() + '.cypher',
+                    startIndex: token.column
+                }))
+                .sort((a, b) => (a.startIndex > b.startIndex ? 1 : -1))
+        }
+    }
+}
